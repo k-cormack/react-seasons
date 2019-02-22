f7119ce03f1db43159717eaa92a975ecaeb46e8b
Keith Cormack <kcormack@definitivelogic.com>
2019-02-21 21:52:55 -0700

    state1

--- src/index.js
+++ src/index.js
@@ -4,13 +4,25 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
-  render() {
+  constructor(props) { // 'constructor' is a keyword
+    super(props);
+
+    // ONLY time this.state is directly assigned without using 'setState' function
+    this.state = { lat: null }; //defining state properties
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        //calling 'setState' here
+        this.setState({ lat: position.coords.latitude }); // 'setState is a function brought in from React.Component
+      },
       (err) => console.log(err)
     );
+  }
 
-    return <div>Latitude: </div>
+
+
+  render() { // 'render' is a funtion brought in from React.Component
+    return <div>Latitude: {this.state.lat}</div>
   }
 }
 
