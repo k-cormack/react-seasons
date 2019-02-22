a5a16726e594110d1c2d578553dcb4172e624478
Keith Cormack <kcormack@definitivelogic.com>
2019-02-21 23:50:18 -0700

    icons

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -10,8 +10,14 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
  const season = getSeason(props.lat, new Date().getMonth());
-
-  return <div>Season: {season}</div>
+ const icon = season === 'Winter' ? 'snowflake' : 'sun';
+  return (
+  <div>
+    <i className={`${icon} icon`} />
+    <h1>{season === 'Winter' ? 'Winter is Coming!' : 'Summer is upon us'}</h1>
+    <i className={`${icon} icon`} />
+  </div>
+  );
 };
 
 export default SeasonDisplay;
