8ca796bf2a3bd0e447579a8b3585fd40117603c3
Keith Cormack <kcormack@definitivelogic.com>
2019-02-22 08:36:21 -0700

    refactor

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,22 +1,35 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: {
+    text: 'Summer is here!',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Winter is Coming!',
+    iconName: 'snowflake'
+  }
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? 'Summer' : 'Winter'; //ternary expression: if true, then....else
+    return lat > 0 ? 'summer' : 'winter'; //ternary expression: if true, then....else
   } else {
-    return lat < 0 ? 'Summer' : 'Winter';
+    return lat < 0 ? 'summer' : 'winter';
   }
 }
 
 const SeasonDisplay = (props) => {
- const season = getSeason(props.lat, new Date().getMonth());
- const icon = season === 'Winter' ? 'snowflake' : 'sun';
-  return (
-  <div>
-    <i className={`${icon} icon`} />
-    <h1>{season === 'Winter' ? 'Winter is Coming!' : 'Summer is upon us'}</h1>
-    <i className={`${icon} icon`} />
-  </div>
+  const season = getSeason(props.lat, new Date().getMonth());
+//  const icon = season === 'Winter' ? 'snowflake' : 'sun';
+  const {text, iconName} = seasonConfig[season]; //returns {text, iconName} corresponding to season
+    return (
+    <div>
+      <i className={`${iconName} icon`} />
+        <h1>Season: {text}</h1>
+          {/* <h1>{season === 'Winter' ? 'Winter is Coming!' : 'Summer is upon us'}</h1> */}
+      <i className={`${iconName} icon`} />
+    </div>
   );
 };
 
