6a28da545881efb6ee60fa074fa5d9bbd2c4254e
Keith Cormack <kcormack@definitivelogic.com>
2019-02-21 22:15:03 -0700

    Conditional Rendering1

--- src/index.js
+++ src/index.js
@@ -8,21 +8,29 @@ class App extends React.Component {
     super(props);
 
     // ONLY time this.state is directly assigned without using 'setState' function
-    this.state = { lat: null }; //defining state properties
+    this.state = { lat: null, errorMessage: '' }; //defining state properties
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         //calling 'setState' here
         this.setState({ lat: position.coords.latitude }); // 'setState is a function brought in from React.Component
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
 
 
   render() { // 'render' is a funtion brought in from React.Component
-    return <div>Latitude: {this.state.lat}</div>
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>LOADING......Please wait....</div>;
   }
 }
 
